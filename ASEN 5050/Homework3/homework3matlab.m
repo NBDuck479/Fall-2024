a72ee7d6bd8019ed4e778e9055989a42c7f93459
unknown <Noel.Duckworth@Colorado.edu>
2024-09-19 20:39:00 -0400

    homework 3 init commit

---
+++ ASEN 5050/Homework3/homework3matlab.m
@@ -0,0 +1,19 @@
+%% Homework 3 
+
+r1 = [-720000; 670000; 310000]; 
+v1 = [2.160; -3.360; 0.620]; 
+
+Gm_saturn = 3.794*10^7; 
+R_saturn  = 60268; 
+
+% get position and velcity at impact 
+
+% get orbital elements and then see what velocity is at rp 
+
+
+
+
+
+
+%% Problem 2: 
+
